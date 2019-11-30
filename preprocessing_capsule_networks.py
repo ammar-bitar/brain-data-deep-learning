97477db66692bfe3d75d122d4c0d26266e94cccd
Ismail Alaoui Abdellaoui <i.alaouiabdellaoui@student.maastrichtuniversity.nl>
2019-11-30 01:39:15 +0100

    Updated the preprocessing function
    not tested yet on our data but should work if you give all the channels in 1 array

--- preprocessing_capsule_networks.py
+++ preprocessing_capsule_networks.py
@@ -6,67 +6,47 @@ Created on Thu Nov 28 03:01:00 2019
 """
 
 import numpy as np
-number_channels = 248
-test = np.arange(1,number_channels,dtype=np.int32)
-test = np.reshape(test,(1,test.shape[0]))
 
-#print(test.shape)
-#print(test)
-output = np.zeros((22,22),dtype=np.int32)
-count = 1
-j = int(output.shape[0]/2)
-#for i in range(int(output.shape[0]/2)):
-#    output[i,j:j+count] = test[i,
-#  count += 2
-#  j -= 1
-#
-##print(output)
-#print(test[0,5:5+1])
-output[0,10:10+1] = test[0,0:0+1]
-output[1,9:9+3] = test[0,1:1+3]
-output[2,8:8+5] = test[0,4:4+5]
-output[3,7:7+7] = test[0,9:9+7]
-output[4,6:6+9] = test[0,16:16+9]
-
-print(output)
-
-
-#j = int(output.shape[0]/2) - 1
-#k = 1
-#l = 0
-#for i in range(int(output.shape[0]/2)):
-#    output[i,j:j+k] = test[0,l:l+k]
-#    j -= 1
-#    l += k
-#    k += 2
-
-i = 10
-j = 11
-k = 23
-l = 121
-#output[10,11:11+23] = test[0,121:121+23]
-#print(len(output[10,0:22]))
-#print(len(test[0,121:121+22]))
-output[10,0:0 + 22] = test[0,121:121+22]
-#output[11,1:1 + 20] = test[0,121+22:121+22+20]
-#output[12,2:2 + 18] = test[0,]
-
-#print(len(output[11,1:1 + 20])) #= 
-#print(output[11,1:1 + 20])
-#print(len(test[0,121+22:121+22+20]))
-#print(test[0,121+22:121+22+20])
-#output[12,2:20] = test[0,118:118+18]
-
-
-
-
-
-
-
-
+def array_to_mesh(array_channels):
+    assert array_channels.shape == (1,248),"the shape of the input array should be (1,248) because there are 248 MEG channels"
+    output = np.zeros((20,22),dtype = np.float)
+    output[0,9:9+3] = array_channels[0,0:0+3]
+    j = int(output.shape[1]/2) - 2
+    k = 3
+    l = 3
+    for i in range(1,int(output.shape[1]/2)):
+        output[i,j:j+k] = array_channels[0,l:l+k]
+        if( i < 10):
+            j -= 1
+            l += k
+            k += 2
+    k = k +1
+    output[10,j:j + k] = array_channels[0,l:l+k]
+    l += k
+    row = 11
+    for i in range(8):
+        output[row,i:i+k] = array_channels[0,l:l+k]
+        row += 1   
+        l += k
+        k -= 2
+    i += 2
+    k-=2
+    output[row,i:i+k] = array_channels[0,l:l+k]
+    return output
     
-#print(output)
 
+#####
+## testing function
+    
+number_channels = 248
+test = np.arange(0,number_channels,dtype=np.int32)
+test[0] = -1 # for visibility, making the first element as -1 because of the 0s
+test_input = np.reshape(test,(1,test.shape[0]))
+output = array_to_mesh(test_input)
+print(output)
+    
+    
+    
 
 
 
