658b9b6b2f5de36b9f9f504cc49b10491db9d20e
Ismail Alaoui Abdellaoui <i.alaouiabdellaoui@student.maastrichtuniversity.nl>
2020-01-06 01:01:02 +0100

    Fixing the generator
    sometimes the rest or the task matrix would be empty, now the load_data function handles those cases
    still need to see how to fix the RAM "issue"

--- CascadeDataGenerator.py
+++ CascadeDataGenerator.py
@@ -13,7 +13,8 @@ class Generator(Sequence):
         self.ram_to_use = ram_to_use
         self.data_type = data_type # will be train or validate or test
         self.number_files = self.get_number_files_to_load()
-        # self.input_dict,self.y = self.load_data()
+        self.input_dict,self.y = self.load_data()
+        
 
     def get_number_files_to_load(self):
         if self.ram_to_use == 1 and self.data_type == "train":
@@ -23,87 +24,131 @@ class Generator(Sequence):
         #do for other cases if working
         return number_files
 
+    def get_dataset_name(self,file_name_with_dir):
+        filename_without_dir = file_name_with_dir.split('/')[-1]
+        temp = filename_without_dir.split('_')[:-1]
+        dataset_name = "_".join(temp)
+        return dataset_name
+
 
     def load_data(self):
         print("data type: {}\nindex_batch_files: {}\nnumber_files: {}".format(self.data_type,self.index_batch_files,self.number_files))
         rest_matrix = np .zeros((248,1))
-
         task_matrix = np .zeros((248,1))
-        # index_batch_files = 4, filestoload = files_paths[4*30:]
-        # print()
+
         files_to_load = self.files_paths[self.index_batch_files * self.number_files: (self.index_batch_files + 1) * self.number_files]
-        print(files_to_load)
-        print(len(files_to_load))
-        if(len(files_to_load) == 1):
-            files_to_load.append(files_to_load[0])
+
         for i in range(len(files_to_load)):
-            if i%2 == 0:
-            # if "rest" in files_to_load[i]:
-                # count_rest_matrix += 1
-                # filename = files_to_load[i].split('/')[-1]
-                f = h5py.File(files_to_load[i],'r')
-                matrix = f['testing_dataset'] # will have to be changed
+            if "rest" in files_to_load[i]:
+                with h5py.File(files_to_load[i],'r') as f:
+                    dataset_name = self.get_dataset_name(files_to_load[i])
+                    matrix = f.get(dataset_name)
+                    matrix = np.array(matrix)
                 assert matrix.shape[0] == 248 , "This rest data does not have 248 channels, but {} instead".format(matrix.shape[0])
                 rest_matrix = np.column_stack((rest_matrix, matrix))
-            # if "task" in files_to_load[i]:
-            if i%2 != 0:
-                # count_task_matrix += 1
-                # filename = files_to_load[i].split('/')[-1]
-                f = h5py.File(files_to_load[i],'r')
-                matrix = f['testing_dataset'] # will have to be changed
+
+            if "task" in files_to_load[i]:
+                with h5py.File(files_to_load[i],'r') as f:
+                    dataset_name = self.get_dataset_name(files_to_load[i])
+                    matrix = f.get(dataset_name)
+                    matrix = np.array(matrix)
                 assert matrix.shape[0] == 248 , "This task data does not have 248 channels, but {} instead".format(matrix.shape[0])
                 task_matrix = np.column_stack((task_matrix, matrix))
+
         rest_matrix = np.delete(rest_matrix, 0, 1)#delete first column made of 0s
         task_matrix = np.delete(task_matrix, 0, 1)#delete first column made of 0s
-        #create the x and y , shuffle it, and return them both
-        # print("rest matrix shape",rest_matrix.shape)
-        rest_matrix = self.normalize_matrix(rest_matrix)
+        if (rest_matrix.shape[1] != 0):
+            rest_is_empty = False
+            print("rest matrix shape",rest_matrix.shape)
+            rest_matrix = self.normalize_matrix(rest_matrix)
+            rest_length = self.closestNumber(int(rest_matrix.shape[1]) - 10,10)
+            rest_meshes = np.zeros((rest_length,20,22))
+            for i in range(rest_length):
+                array_time_step = np.reshape(rest_matrix[:,i],(1,248))
+                rest_meshes[i] = self.array_to_mesh(array_time_step)
 
-        task_matrix = self.normalize_matrix(task_matrix)
+            del rest_matrix
+            gc.collect()
+            input1_rest,input2_rest,input3_rest,input4_rest,input5_rest = self.get_input_lists(rest_meshes, self.get_lists_indexes(rest_length))
+            del rest_meshes
+            gc.collect()
+            number_y_labels_rest = int(rest_length/5)
+            y_rest = np.zeros((number_y_labels_rest,1),dtype=np.int8)
+        else:
+            rest_is_empty = True
 
-        rest_length = self.closestNumber(int(rest_matrix.shape[1]) - 10,10)
-        task_length = self.closestNumber(int(task_matrix.shape[1]) - 10,10)
+        if (task_matrix.shape[1] != 0):
+            task_is_empty = False
+            print("task matrix shape",task_matrix.shape)
+            task_matrix = self.normalize_matrix(task_matrix)           
+            task_length = self.closestNumber(int(task_matrix.shape[1]) - 10,10)
+            task_meshes = np.zeros((task_length,20,22))
+            for i in range(task_length):
+                array_time_step = np.reshape(task_matrix[:,i],(1,248))
+                task_meshes[i] = self.array_to_mesh(array_time_step)
 
-        rest_meshes = np.zeros((rest_length,20,22))
-        for i in range(rest_length):
-            # print(rest_matrix[i].shape)
-            # print(rest_matrix[:,i].shape)
-            array_time_step = np.reshape(rest_matrix[:,i],(1,248))
-            rest_meshes[i] = self.array_to_mesh(array_time_step)
-
-        del rest_matrix
-        gc.collect()
-
-        task_meshes = np.zeros((task_length,20,22))
-        for i in range(task_length):
-            # print(task_matrix[i].shape)
-            # print(task_matrix[:,i].shape)
-            array_time_step = np.reshape(task_matrix[:,i],(1,248))
-            task_meshes[i] = self.array_to_mesh(array_time_step)
-
-        del task_matrix
-        gc.collect()
-
-        number_y_labels_rest = int(rest_length/5)
-        y_rest = np.zeros((number_y_labels_rest,1),dtype=np.int8)
-
-        number_y_labels_task = int(task_length/5)
-        y_task = np.ones((number_y_labels_task,1),dtype=np.int8)
+            del task_matrix
+            gc.collect()
+            input1_task,input2_task,input3_task,input4_task,input5_task = self.get_input_lists(task_meshes, self.get_lists_indexes(task_length))
+            del task_meshes
+            gc.collect()
+            number_y_labels_task = int(task_length/5)
+            y_task = np.ones((number_y_labels_task,1),dtype=np.int8)
+        else:
+            task_is_empty = True
 
 
-        input1_rest,input2_rest,input3_rest,input4_rest,input5_rest = self.get_input_lists(rest_meshes, self.get_lists_indexes(rest_length))
-        del rest_meshes
-        gc.collect()
+        if(not rest_is_empty and not task_is_empty):
+            input1 = np.concatenate((input1_rest,input1_task))
+            input2 = np.concatenate((input2_rest,input2_task))
+            input3 = np.concatenate((input3_rest,input3_task))
+            input4 = np.concatenate((input4_rest,input4_task))
+            input5 = np.concatenate((input5_rest,input5_task))
 
-        input1_task,input2_task,input3_task,input4_task,input5_task = self.get_input_lists(task_meshes, self.get_lists_indexes(task_length))
-        del task_meshes
-        gc.collect()
+            y = np.concatenate((y_rest,y_task))
 
-        input1 = np.concatenate((input1_rest,input1_task))
-        input2 = np.concatenate((input2_rest,input2_task))
-        input3 = np.concatenate((input3_rest,input3_task))
-        input4 = np.concatenate((input4_rest,input4_task))
-        input5 = np.concatenate((input5_rest,input5_task))
+            del input1_rest
+            del input2_rest
+            del input3_rest
+            del input4_rest
+            del input5_rest
+
+            del input1_task
+            del input2_task
+            del input3_task
+            del input4_task
+            del input5_task
+
+        elif rest_is_empty:
+            input1 = input1_task
+            input2 = input2_task
+            input3 = input3_task
+            input4 = input4_task
+            input5 = input5_task
+            
+            y = y_task
+
+            del input1_task
+            del input2_task
+            del input3_task
+            del input4_task
+            del input5_task
+
+        elif task_is_empty:
+            input1 = input1_rest
+            input2 = input2_rest
+            input3 = input3_rest
+            input4 = input4_rest
+            input5 = input5_rest
+            
+            y = y_rest
+
+            del input1_rest
+            del input2_rest
+            del input3_rest
+            del input4_rest
+            del input5_rest
+
 
         input1 = np.reshape(input1,(input1.shape[0],20,22,1))
         input2 = np.reshape(input2,(input2.shape[0],20,22,1))
@@ -111,22 +156,7 @@ class Generator(Sequence):
         input4 = np.reshape(input4,(input4.shape[0],20,22,1))
         input5 = np.reshape(input5,(input5.shape[0],20,22,1))
 
-
-        del input1_rest
-        del input2_rest
-        del input3_rest
-        del input4_rest
-        del input5_rest
-
-        del input1_task
-        del input2_task
-        del input3_task
-        del input4_task
-        del input5_task
-
         gc.collect()
-
-        y = np.concatenate((y_rest,y_task))
 
         #Shuffling the data
         input1,input2,input3,input4,input5,y = shuffle(input1,input2,input3,input4,input5, y, random_state=42)
@@ -170,7 +200,9 @@ class Generator(Sequence):
         # return math.ceil(len(self.files_paths) / self.number_files)
 
     def __getitem__(self, index): # returns a batch
+        gc.collect()
         batch_input, batch_output = self.load_data()
+        # batch_input, batch_output = self.load_synthetic_data()
         self.index_batch_files += 1
         return batch_input,batch_output
     
@@ -202,7 +234,7 @@ class Generator(Sequence):
         output[row,i:i+k] = array_channels[0,l:l+k]
         return output
 
-    #Given n, find the closest number that divides n by m ( used for an even splitting of the data)
+    #Given n, find the closest number that divides n by m ( used for the splitting by 5) m will generally be 5 or 10
     def closestNumber(self,n, m) : 
         q = int(n / m) 
         n1 = m * q 
